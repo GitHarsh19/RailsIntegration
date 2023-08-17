32b9e5dbaa0bfbc4204a766879f95d4d46e77213
harshit <harshitagarwalsmt@gmail.com>
2023-08-18 01:10:19 +0530

    successfull integrated

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,10 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "counterup"
+pin "easing"
+pin "main"
+pin "owlcarousel"
+pin "waypoints"
+pin "wow"
+
